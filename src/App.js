cdb6ef222caaffaed18602378301b08492db178b
Akhil-Kulal <akhilkulal9@gmail.com>
2022-09-30 10:28:02 +0530

    Add link for routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import HabitList from "./components/HabitList";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import AddHabit from "./components/AddHabit";
 
@@ -7,10 +7,16 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Link to="/addHabit" className="navigate">
+        Add Habit
+      </Link>
+      <Link to="/" className="navigate">
+        Habits
+      </Link>
 
       <Routes>
         <Route path="/addHabit" element={<AddHabit />} />
-        <Route path="/HabitList" element={<HabitList />} />
+        <Route path="/" element={<HabitList />} />
       </Routes>
     </BrowserRouter>
   );
